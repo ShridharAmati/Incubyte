120cffcb8d2896f37eafa11ac69fb2d2d509b9c1
Shridhar <70563478+ShridharAmati@users.noreply.github.com>
2022-02-05 16:08:53 +0530

    Create StringCalculator.java
    for no input

---
+++ StringCalculator.java
@@ -0,0 +1,12 @@
+public class Stringcalculator {
+	int Add(String numbers) {
+		if(numbers=="") {
+		return 0;
+	}
+		return 0;
+		}
+	public static void main(String[] args) {	
+		String numbers="";
+	}
+
+}
